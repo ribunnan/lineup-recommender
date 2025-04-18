1c0e8eebb9f9f40a2d7a1a3aa3693632ed37646c
ribunnan <165739167+ribunnan@users.noreply.github.com>
2025-04-18 14:30:00 +0900

    Add files via upload

---
+++ docs/rolesData.js
@@ -0,0 +1,206 @@
+const rolesData = [
+  {
+    "name": "乞讨者",
+    "image": "images/角色/乞讨者.jpg"
+  },
+  {
+    "name": "剑术导师",
+    "image": "images/角色/剑术导师.jpg"
+  },
+  {
+    "name": "占卜师",
+    "image": "images/角色/占卜师.jpg"
+  },
+  {
+    "name": "占星师",
+    "image": "images/角色/占星师.jpg"
+  },
+  {
+    "name": "厨师学徒",
+    "image": "images/角色/厨师学徒.jpg"
+  },
+  {
+    "name": "变色龙大师",
+    "image": "images/角色/变色龙大师.jpg"
+  },
+  {
+    "name": "古堡三杰",
+    "image": "images/角色/古堡三杰.jpg"
+  },
+  {
+    "name": "圣诞矮人",
+    "image": "images/角色/圣诞矮人.jpg"
+  },
+  {
+    "name": "地灵殉道者",
+    "image": "images/角色/地灵殉道者.jpg"
+  },
+  {
+    "name": "地灵监工",
+    "image": "images/角色/地灵监工.jpg"
+  },
+  {
+    "name": "天才发明家",
+    "image": "images/角色/天才发明家.jpg"
+  },
+  {
+    "name": "小猪建筑师",
+    "image": "images/角色/小猪建筑师.jpg"
+  },
+  {
+    "name": "小猫进货商",
+    "image": "images/角色/小猫进货商.jpg"
+  },
+  {
+    "name": "巧手化妆师",
+    "image": "images/角色/巧手化妆师.jpg"
+  },
+  {
+    "name": "幽灵狗",
+    "image": "images/角色/幽灵狗.jpg"
+  },
+  {
+    "name": "影魔",
+    "image": "images/角色/影魔.jpg"
+  },
+  {
+    "name": "批发商人",
+    "image": "images/角色/批发商人.jpg"
+  },
+  {
+    "name": "掌灯者",
+    "image": "images/角色/掌灯者.jpg"
+  },
+  {
+    "name": "掘墓人",
+    "image": "images/角色/掘墓人.jpg"
+  },
+  {
+    "name": "时装设计师",
+    "image": "images/角色/时装设计师.jpg"
+  },
+  {
+    "name": "星雷女巫",
+    "image": "images/角色/星雷女巫.jpg"
+  },
+  {
+    "name": "暴风女士",
+    "image": "images/角色/暴风女士.jpg"
+  },
+  {
+    "name": "机械魔术师",
+    "image": "images/角色/机械魔术师.jpg"
+  },
+  {
+    "name": "杂货商人",
+    "image": "images/角色/杂货商人.jpg"
+  },
+  {
+    "name": "森灵",
+    "image": "images/角色/森灵.jpg"
+  },
+  {
+    "name": "流浪佣兵",
+    "image": "images/角色/流浪佣兵.jpg"
+  },
+  {
+    "name": "火精灵",
+    "image": "images/角色/火精灵.jpg"
+  },
+  {
+    "name": "独眼调查员",
+    "image": "images/角色/独眼调查员.jpg"
+  },
+  {
+    "name": "狼王",
+    "image": "images/角色/狼王.jpg"
+  },
+  {
+    "name": "玩具商贩",
+    "image": "images/角色/玩具商贩.jpg"
+  },
+  {
+    "name": "皇家指挥官",
+    "image": "images/角色/皇家指挥官.jpg"
+  },
+  {
+    "name": "精灵女王",
+    "image": "images/角色/精灵女王.jpg"
+  },
+  {
+    "name": "缝合怪",
+    "image": "images/角色/缝合怪.jpg"
+  },
+  {
+    "name": "能量装置",
+    "image": "images/角色/能量装置.jpg"
+  },
+  {
+    "name": "蜡烛人",
+    "image": "images/角色/蜡烛人.jpg"
+  },
+  {
+    "name": "调酒师",
+    "image": "images/角色/调酒师.jpg"
+  },
+  {
+    "name": "贪婪地精",
+    "image": "images/角色/贪婪地精.jpg"
+  },
+  {
+    "name": "追梦旅人",
+    "image": "images/角色/追梦旅人.jpg"
+  },
+  {
+    "name": "铸剑工匠",
+    "image": "images/角色/铸剑工匠.jpg"
+  },
+  {
+    "name": "雕塑家",
+    "image": "images/角色/雕塑家.jpg"
+  },
+  {
+    "name": "雷精灵",
+    "image": "images/角色/雷精灵.jpg"
+  },
+  {
+    "name": "香水小姐",
+    "image": "images/角色/香水小姐.jpg"
+  },
+  {
+    "name": "驯鹿勇士",
+    "image": "images/角色/驯鹿勇士.jpg"
+  },
+  {
+    "name": "魔法树丛",
+    "image": "images/角色/魔法树丛.jpg"
+  },
+  {
+    "name": "鱼美人",
+    "image": "images/角色/鱼美人.jpg"
+  },
+  {
+    "name": "鲨鱼恶霸",
+    "image": "images/角色/鲨鱼恶霸.jpg"
+  },
+  {
+    "name": "黑天鹅",
+    "image": "images/角色/黑天鹅.jpg"
+  },
+  {
+    "name": "黑市商贩",
+    "image": "images/角色/黑市商贩.jpg"
+  },
+  {
+    "name": "黑猫",
+    "image": "images/角色/黑猫.jpg"
+  },
+  {
+    "name": "鼹鼠奸商",
+    "image": "images/角色/鼹鼠奸商.jpg"
+  },
+  {
+    "name": "齿轮绅士",
+    "image": "images/角色/齿轮绅士.jpg"
+  }
+];
