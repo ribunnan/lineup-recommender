2e87e99bce260af7a1e9d41a6020436017d81f8e
ribunnan <165739167+ribunnan@users.noreply.github.com>
2025-04-18 14:48:33 +0900

    Add files via upload

---
+++ docs/equipData.js
@@ -0,0 +1,290 @@
+const equipData = [
+  {
+    "name": "亡灵饼干",
+    "image": "images/装备/亡灵饼干.jpg"
+  },
+  {
+    "name": "亡者之盾",
+    "image": "images/装备/亡者之盾.jpg"
+  },
+  {
+    "name": "伐木斧",
+    "image": "images/装备/伐木斧.jpg"
+  },
+  {
+    "name": "凡人的意志",
+    "image": "images/装备/凡人的意志.jpg"
+  },
+  {
+    "name": "刃弩",
+    "image": "images/装备/刃弩.jpg"
+  },
+  {
+    "name": "剧毒匕首",
+    "image": "images/装备/剧毒匕首.jpg"
+  },
+  {
+    "name": "双头剑",
+    "image": "images/装备/双头剑.jpg"
+  },
+  {
+    "name": "发电机",
+    "image": "images/装备/发电机.jpg"
+  },
+  {
+    "name": "噬魂镰",
+    "image": "images/装备/噬魂镰.jpg"
+  },
+  {
+    "name": "圆盾",
+    "image": "images/装备/圆盾.jpg"
+  },
+  {
+    "name": "塔盾",
+    "image": "images/装备/塔盾.jpg"
+  },
+  {
+    "name": "天使手镜",
+    "image": "images/装备/天使手镜.jpg"
+  },
+  {
+    "name": "夺命双刃",
+    "image": "images/装备/夺命双刃.jpg"
+  },
+  {
+    "name": "女巫坩埚",
+    "image": "images/装备/女巫坩埚.jpg"
+  },
+  {
+    "name": "尖刺壁垒",
+    "image": "images/装备/尖刺壁垒.jpg"
+  },
+  {
+    "name": "尖刺羽毛",
+    "image": "images/装备/尖刺羽毛.jpg"
+  },
+  {
+    "name": "带毒藤蔓",
+    "image": "images/装备/带毒藤蔓.jpg"
+  },
+  {
+    "name": "幽暗烛光",
+    "image": "images/装备/幽暗烛光.jpg"
+  },
+  {
+    "name": "弹幕射击",
+    "image": "images/装备/弹幕射击.jpg"
+  },
+  {
+    "name": "弹弓",
+    "image": "images/装备/弹弓.jpg"
+  },
+  {
+    "name": "彼岸花",
+    "image": "images/装备/彼岸花.jpg"
+  },
+  {
+    "name": "快刀",
+    "image": "images/装备/快刀.jpg"
+  },
+  {
+    "name": "怀表",
+    "image": "images/装备/怀表.jpg"
+  },
+  {
+    "name": "恶魔刀刃",
+    "image": "images/装备/恶魔刀刃.jpg"
+  },
+  {
+    "name": "战斗法杖",
+    "image": "images/装备/战斗法杖.jpg"
+  },
+  {
+    "name": "抄写笔",
+    "image": "images/装备/抄写笔.jpg"
+  },
+  {
+    "name": "护盾",
+    "image": "images/装备/护盾.jpg"
+  },
+  {
+    "name": "护身符",
+    "image": "images/装备/护身符.jpg"
+  },
+  {
+    "name": "提灯",
+    "image": "images/装备/提灯.jpg"
+  },
+  {
+    "name": "斩龙斧",
+    "image": "images/装备/斩龙斧.jpg"
+  },
+  {
+    "name": "旋风斧",
+    "image": "images/装备/旋风斧.jpg"
+  },
+  {
+    "name": "暗藏炸弹",
+    "image": "images/装备/暗藏炸弹.jpg"
+  },
+  {
+    "name": "月光法典",
+    "image": "images/装备/月光法典.jpg"
+  },
+  {
+    "name": "朴素法袍",
+    "image": "images/装备/朴素法袍.jpg"
+  },
+  {
+    "name": "机械手臂",
+    "image": "images/装备/机械手臂.jpg"
+  },
+  {
+    "name": "棱镜",
+    "image": "images/装备/棱镜.jpg"
+  },
+  {
+    "name": "死亡钟摆",
+    "image": "images/装备/死亡钟摆.jpg"
+  },
+  {
+    "name": "死神之镰",
+    "image": "images/装备/死神之镰.jpg"
+  },
+  {
+    "name": "殉道护符",
+    "image": "images/装备/殉道护符.jpg"
+  },
+  {
+    "name": "殉道铠甲",
+    "image": "images/装备/殉道铠甲.jpg"
+  },
+  {
+    "name": "法力护盾",
+    "image": "images/装备/法力护盾.jpg"
+  },
+  {
+    "name": "法力翡翠",
+    "image": "images/装备/法力翡翠.jpg"
+  },
+  {
+    "name": "法师护手",
+    "image": "images/装备/法师护手.jpg"
+  },
+  {
+    "name": "活力护符",
+    "image": "images/装备/活力护符.jpg"
+  },
+  {
+    "name": "流血箭",
+    "image": "images/装备/流血箭.jpg"
+  },
+  {
+    "name": "满月",
+    "image": "images/装备/满月.jpg"
+  },
+  {
+    "name": "潜行者手套",
+    "image": "images/装备/潜行者手套.jpg"
+  },
+  {
+    "name": "火苗",
+    "image": "images/装备/火苗.jpg"
+  },
+  {
+    "name": "灵魂结晶",
+    "image": "images/装备/灵魂结晶.jpg"
+  },
+  {
+    "name": "灼热斗篷",
+    "image": "images/装备/灼热斗篷.jpg"
+  },
+  {
+    "name": "焊接器",
+    "image": "images/装备/焊接器.jpg"
+  },
+  {
+    "name": "猪皮背包",
+    "image": "images/装备/猪皮背包.jpg"
+  },
+  {
+    "name": "玩具长枪",
+    "image": "images/装备/玩具长枪.jpg"
+  },
+  {
+    "name": "生命护符",
+    "image": "images/装备/生命护符.jpg"
+  },
+  {
+    "name": "疯狂头骨",
+    "image": "images/装备/疯狂头骨.jpg"
+  },
+  {
+    "name": "盾墙",
+    "image": "images/装备/盾墙.jpg"
+  },
+  {
+    "name": "短剑",
+    "image": "images/装备/短剑.jpg"
+  },
+  {
+    "name": "研究图纸",
+    "image": "images/装备/研究图纸.jpg"
+  },
+  {
+    "name": "袖箭",
+    "image": "images/装备/袖箭.jpg"
+  },
+  {
+    "name": "远古十字架",
+    "image": "images/装备/远古十字架.jpg"
+  },
+  {
+    "name": "速度之靴",
+    "image": "images/装备/速度之靴.jpg"
+  },
+  {
+    "name": "重型弩炮",
+    "image": "images/装备/重型弩炮.jpg"
+  },
+  {
+    "name": "重斧",
+    "image": "images/装备/重斧.jpg"
+  },
+  {
+    "name": "金匕首",
+    "image": "images/装备/金匕首.jpg"
+  },
+  {
+    "name": "长弓",
+    "image": "images/装备/长弓.jpg"
+  },
+  {
+    "name": "风之石",
+    "image": "images/装备/风之石.jpg"
+  },
+  {
+    "name": "飓风法杖",
+    "image": "images/装备/飓风法杖.jpg"
+  },
+  {
+    "name": "飞斧",
+    "image": "images/装备/飞斧.jpg"
+  },
+  {
+    "name": "饥饿魔典",
+    "image": "images/装备/饥饿魔典.jpg"
+  },
+  {
+    "name": "鬼摇铃",
+    "image": "images/装备/鬼摇铃.jpg"
+  },
+  {
+    "name": "鹿角",
+    "image": "images/装备/鹿角.jpg"
+  },
+  {
+    "name": "龙之宝珠",
+    "image": "images/装备/龙之宝珠.jpg"
+  }
+];
